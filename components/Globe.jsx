71b54d7451d063260374f2c72b3305e1e6e7cc51
golubhattuk01 <148994632+golubhattuk01@users.noreply.github.com>
2024-08-24 11:23:49 +0530

    pointer updated

--- components/Globe.jsx
+++ components/Globe.jsx
@@ -1,35 +1,44 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import { useAuth0 } from "@auth0/auth0-react";
-
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 const SERVER_URL = import.meta.env.VITE_SOCKET_SERVER;
 
 const socket = io(SERVER_URL);
 
+// Fix Leaflet's default icon paths
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 const Globe = () => {
-  const { user, isAuthenticated, isLoading, loginWithRedirect, logout } =
-    useAuth0();
+  const { user, isAuthenticated } = useAuth0();
   const [clients, setClients] = useState([]);
   const [userLocation, setUserLocation] = useState([23, 79]);
 
   useEffect(() => {
     const interval = setInterval(() => {
       loc_share();
-    }, 5000); // Update every 5 seconds
+    }, 5000);
 
     socket.on("allLocations", (data) => {
       setClients(data);
-      // Optionally, update the map with new locations
     });
 
-    return () => clearInterval(interval); // Cleanup the interval on component unmount
+    return () => clearInterval(interval);
   }, []);
 
   const loc_share = () => {
-    console.log("running every 5 seconds");
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         const latitude = position.coords.latitude;
@@ -39,7 +48,7 @@ const Globe = () => {
           l2: longitude,
           name: user.name,
         });
-        setUserLocation([latitude, longitude]); // Update user location state
+        setUserLocation([latitude, longitude]);
       });
     } else {
       console.error("Geolocation is not supported by this browser.");
@@ -63,18 +72,15 @@ const Globe = () => {
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-
             <Marker position={userLocation}>
               <Popup>{user.name}</Popup>
             </Marker>
 
-            {clients.map(({ id, l1, l2, name }) => {
-              return (
-                <Marker key={id} position={[l1, l2]}>
-                  <Popup>{name} is here in the map</Popup>
-                </Marker>
-              );
-            })}
+            {clients.map(({ id, l1, l2, name }) => (
+              <Marker key={id} position={[l1, l2]}>
+                <Popup>{name} is here in the map</Popup>
+              </Marker>
+            ))}
           </MapContainer>
         </div>
       ) : (
