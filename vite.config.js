522f6318e6e893ebc7798ee93f607afacd78326a
Gaurav Bhatt <gbhatt570@gmail.com>
2025-07-27 10:08:16 +0530

    Update allowedHosts in vite.config.js to restrict to specific domain for improved security

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts:["*.golu.codes"],
+    allowedHosts:["way.golu.codes"],
     watch: {
       usePolling: true,
     },
