044085ae8ed4f604d457bff108b7b0674c6f0dbb
Gaurav Bhatt <gbhatt570@gmail.com>
2025-08-21 15:53:08 +0530

    feat: add preview server configuration for external accessibility

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,9 @@ export default defineConfig({
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 5173, // you can replace this port with any port
-  }
+  },
+    preview: {
+    port: 5173, // change to your desired port
+    host: '0.0.0.0', // optional: to make it accessible externally
+  },
 })
